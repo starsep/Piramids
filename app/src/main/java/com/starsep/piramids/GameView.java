e9d241a6839b56eaf66fb3bb0e1dc06122306d87
Filip Czaplicki <fc359081@students.mimuw.edu.pl>
2015-08-13 17:33:16 +0200

    Graphics beginning

--- app/src/main/java/com/starsep/piramids/GameView.java
+++ app/src/main/java/com/starsep/piramids/GameView.java
@@ -24,5 +24,17 @@ public class GameView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+
+        float maxSize = Math.min(getHeight(), getWidth()) - 1f;
+        float boardSize = 5; //TODO
+
+        for(float q = 0f; q <= maxSize; q += maxSize / boardSize) {
+            canvas.drawLine(0, q, maxSize, q, paint);
+            canvas.drawLine(q, 0, q, maxSize, paint);
+        }
+
+
+        super.onDraw(canvas);
     }
 }
