994fa48eb26dd6b0bbaa4dc231b27878a8099fe8
Filip Czaplicki <fc359081@students.mimuw.edu.pl>
2015-08-15 02:58:37 +0200

    Highlight buttons

--- app/src/main/java/com/starsep/piramids/GameView.java
+++ app/src/main/java/com/starsep/piramids/GameView.java
@@ -31,14 +31,21 @@ public class GameView extends LinearLayout {
         public class GameViewElement extends Button {
             private int x;
             private int y;
+
             public GameViewElement(Context context, int x, int y) {
                 super(context);
                 this.x = x;
                 this.y = y;
-                ((Activity)getContext()).registerForContextMenu(this);
+                ((Activity) getContext()).registerForContextMenu(this);
                 setBackgroundResource(R.drawable.game_button);
                 setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 1.0f));
                 setOrientation(VERTICAL);
+                setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        chooseButton(v);
+                    }
+                });
             }
 
             //TODO: Awful hack, delete
@@ -60,7 +67,7 @@ public class GameView extends LinearLayout {
             super(context);
             elements = new GameViewElement[size];
             addView(new HintView(gameBoard.getLeftHints()[x], getContext()));
-            for(int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) {
                 elements[i] = new GameViewElement(getContext(), x, i);
                 addView(elements[i]);
             }
@@ -73,15 +80,26 @@ public class GameView extends LinearLayout {
     private GameViewRow[] rows;
     private GameBoard gameBoard;
 
+    public void chooseButton(View view) {
+        Button button = (Button) view;
+        int x = (int) button.getX();
+        int y = (int) button.getY();
+        for (int i = 0; i < rows.length; i++) {
+            for (int j = 0; j < rows[i].elements.length; j++) {
+                Button element = rows[i].elements[j];
+                element.setBackgroundResource(i == x || j == y ? R.drawable.game_highlighted_button : R.drawable.game_button);
+            }
+        }
+        button.setBackgroundResource(R.drawable.game_chosen_button);
+    }
+
     public GameView(Context context) {
         super(context);
-        int minSize = Math.min(getWidth(), getHeight());
-        setLayoutParams(new LinearLayout.LayoutParams(minSize, minSize, 0.0f));
         setOrientation(VERTICAL);
     }
 
-    public GameView(Context context, AttributeSet attributeSet) {
-        super(context, attributeSet);
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
         setOrientation(VERTICAL);
     }
 
@@ -91,9 +109,9 @@ public class GameView extends LinearLayout {
     }
 
     public void refresh() {
-        for(int x =  0; x < rows.length; x++)
-            for(int y = 0; y < rows[x].elements.length; y++)
-                if(gameBoard.getTile(x, y) != 0)
+        for (int x = 0; x < rows.length; x++)
+            for (int y = 0; y < rows[x].elements.length; y++)
+                if (gameBoard.getTile(x, y) != 0)
                     rows[x].elements[y].setText(String.valueOf(gameBoard.getTile(x, y)));
                 else rows[x].elements[y].setText("");
     }
@@ -101,7 +119,7 @@ public class GameView extends LinearLayout {
     public LinearLayout makeHorizontalHints(int[] source) {
         LinearLayout result = new LinearLayout(getContext());
         result.setOrientation(HORIZONTAL);
-        for(int hint : source)
+        for (int hint : source)
             result.addView(new HintView(hint, getContext()));
         return result;
     }
@@ -109,11 +127,11 @@ public class GameView extends LinearLayout {
     public void setGameBoard(GameBoard gameBoard) {
         this.gameBoard = gameBoard;
         rows = new GameViewRow[gameBoard.getSize()];
-        for(int i = 0; i < rows.length; i++) {
+        for (int i = 0; i < rows.length; i++) {
             rows[i] = new GameViewRow(getContext(), gameBoard.getSize(), i);
         }
         addView(makeHorizontalHints(gameBoard.getUpHints()));
-        for(GameViewRow row : rows) {
+        for (GameViewRow row : rows) {
             addView(row);
         }
         addView(makeHorizontalHints(gameBoard.getDownHints()));
